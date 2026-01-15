ca35214ba28f9f70f8be03e38f9092fde676d098
Ayuu <ayushks2805@gmail.com>
2026-01-15 18:47:34 +0530

    on changing one app component only that app build and show changes in multi apps project explaination

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -3,8 +3,10 @@ import { Button } from '@repo/ui';
 function App() {
   return (
      <div>
-      <h2>Web page</h2>
+      <h2><span style={{color: "red"}}>Web</span> page</h2>
+      <h4>Monorepo example</h4>
       <Button>Login from web</Button>
+      <p>login to connect with us....</p>
      </div>
   );
 }
